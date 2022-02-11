5aec08ae1ea3d212be0c52fdc4fc4ae112bfa20a
l3miage-baht <bahalioufgn@gmail.com>
2022-02-11 11:36:53 +0100

    Version 1 plus plus : Pour tester rapidement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { TodolistService, TodoList, TodoItem } from './todolist.service';
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'l3m-tpX-todolist-angular-y2022';
+
+  constructor(private todoListService: TodolistService){
+  }
+
+  get ObsTodoService(): Observable<TodoList>{
+    return this.todoListService.observable;
+  }
+
+  submitForm(v: string): void{
+    this.todoListService.create(v);
+    console.log("V en param est : "+ v);
+
+    console.log("Formulaire soumi ");
+    console.log(this.todoListService);
+  }
+
+  update(data: Partial<TodoItem>, ...items: readonly TodoItem[]): void{
+    this.todoListService.update(data, ...items);
+  }
+
+  delete(item: TodoItem): void{
+    this.todoListService.delete(item);
+  }
+
+  trackById(n: number, i: TodoItem): number{
+    return i.id;
+  }
 }
