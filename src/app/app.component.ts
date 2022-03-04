c67568314b82510330ec4c9e5381281c7a8a2f32
l3miage-baht <bahalioufgn@gmail.com>
2022-03-04 09:54:59 +0100

    Version 1 plus plus : Pour un meilleur service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,37 @@
 import { TodolistService, TodoList, TodoItem } from './todolist.service';
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, tap, BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
   title = 'l3m-tpX-todolist-angular-y2022';
+  //private subj = new BehaviorSubject<TodoList>({label: 'L3 MIAGE', items: [] });
+   //bs = new BehaviorSubject<TodoList>({label: 'L3 MIAGE', items: [] })
 
   constructor(private todoListService: TodolistService){
+
+  }
+
+  ngOnDestroy(): void {
+    this.fillLocalStorage()
   }
 
   get ObsTodoService(): Observable<TodoList>{
     return this.todoListService.observable;
+  }
+
+
+  fillLocalStorage(): void{
+    this.ObsTodoService.subscribe((TDS) => {
+      localStorage.setItem('TODO', JSON.stringify(TDS))
+      //localStorage.getItem('TODO')
+      // JSON.parse(localStorage.getItem('TODO'))
+    })
   }
 
   submitForm(v: string): void{
@@ -24,14 +40,17 @@ export class AppComponent {
 
     console.log("Formulaire soumi ");
     console.log(this.todoListService);
+    this.fillLocalStorage()
   }
 
   update(data: Partial<TodoItem>, ...items: readonly TodoItem[]): void{
     this.todoListService.update(data, ...items);
+    this.fillLocalStorage();
   }
 
   delete(item: TodoItem): void{
     this.todoListService.delete(item);
+    this.fillLocalStorage();
   }
 
   trackById(n: number, i: TodoItem): number{
